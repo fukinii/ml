0c7c5c222d1c13bb00d394f2ab3e5cb9469485c2
fukinii <fookinilia2013@gmail.com>
2021-10-13 17:31:27 +0300

    Фикс бага с классификацией

--- task_2/lr.py
+++ task_2/lr.py
@@ -1,18 +1,19 @@
 import numpy as np
 import torch as T
 
-class LogisticRegression:
-    
+
+class MyLogisticRegression:
+
     def __init__(self, device):
-        self.device=device
+        self.device = device
         self.w: T.tensor = T.zeros((2), dtype=T.float32, requires_grad=True).to(device)
         self.b: T.tensor = T.zeros((1), dtype=T.float32, requires_grad=True).to(device)
-                
+
     @staticmethod
     def forward(
-        x: T.tensor, 
-        w: T.tensor, 
-        b: T.tensor):
+            x: T.tensor,
+            w: T.tensor,
+            b: T.tensor):
         """
         Функция для нахождения сигмоиды 
 
@@ -26,14 +27,15 @@ class LogisticRegression:
         z += b
         p = 1 / (1 + T.exp(-z))
         return p
-    
-    def random_w_and_b(self): 
+
+    def random_w_and_b(self):
         """
         Метод для рандомизации (начального задания) коэффициентов w и b
         """
-        
+
         # границы для случайной величины
-        lo = -0.01; hi = 0.01
+        lo = -0.01
+        hi = 0.01
 
         # Начинаем со случайных величин
         # 2 - 2 признака, low, high - границы сверху и снизу
@@ -51,18 +53,17 @@ class LogisticRegression:
         b.grad = T.zeros(1)
         b.retain_grad()
         return w, b
-    
-    
+
     def fit(
-        self,
-        train_x: T.Tensor,
-        train_y: T.Tensor,
-        lrn_rate: np.double,
-        indices: np.array,
-        num_of_epoch_iterations: int,
-        regular: int = 0
+            self,
+            train_x: T.Tensor,
+            train_y: T.Tensor,
+            lrn_rate: np.double,
+            indices: np.array,
+            num_of_epoch_iterations: int,
+            regular: int = 0
     ) -> T.Tensor:
-        
+
         """Метод для обучения выборки и нахождения коэффициентов w. Оптимизирующий метод - градиентный спуск
 
         Args:
@@ -72,12 +73,12 @@ class LogisticRegression:
         w, b = self.random_w_and_b()
 
         for epoch in range(0, num_of_epoch_iterations):
-            tot_loss = 0
+            # tot_loss = 0
             tot_loss = T.zeros((1), dtype=T.float32, requires_grad=True).to(self.device)
             tot_loss.grad = T.zeros(1)
             tot_loss.retain_grad()
 
-            np.random.shuffle(indices)  
+            np.random.shuffle(indices)
 
             for ii in range(len(indices)):
                 i = indices[ii]
@@ -91,10 +92,10 @@ class LogisticRegression:
                 loss = (oupt - target).pow(2).sum()
 
                 # Суммарный loss
-                tot_loss = loss+tot_loss
+                tot_loss = loss + tot_loss
 
-#             tot_loss = tot_loss + T.norm(w, p=2) # l2 reg
-#             tot_loss = tot_loss + T.norm(w, p=1) # l1 reg
+            #             tot_loss = tot_loss + T.norm(w, p=2) # l2 reg
+            # tot_loss = tot_loss + T.norm(w, p=1) # l1 reg
 
             tot_loss.backward(retain_graph=True)  # compute gradients
 
@@ -104,25 +105,21 @@ class LogisticRegression:
             w.grad = T.zeros(2)
             b.grad = T.zeros(1)
 
-#             if epoch % 10 == 0:
-#                 print("epoch = %4d " % epoch, end="")
-#                 print("   loss = %6.4f" % (tot_loss / 6))
+        #             if epoch % 10 == 0:
+        #                 print("epoch = %4d " % epoch, end="")
+        #                 print("   loss = %6.4f" % (tot_loss / 6))
 
         self.w = w
         self.b = b
-                
+
         return w, b
 
-    
     def predict(self, x):
 
-        p = np.empty(len(x), dtype = np.float32)
+        p = np.empty(len(x), dtype=np.float32)
         i = 0
         for x_i in x:
             p[i] = self.forward(x_i, self.w, self.b)
-            i = i + 1    
-        
+            i = i + 1
+
         return p
-        
-        
-        
